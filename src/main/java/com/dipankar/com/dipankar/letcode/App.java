f9846c8a16669a2e0911ddb7eb40224cb9d5c9a9
dipankarpl <dipankarp826@yahoo.com>
2021-02-01 18:24:30 +0530

    commiting empty project

---
+++ src/main/java/com/dipankar/com/dipankar/letcode/App.java
@@ -0,0 +1,13 @@
+package com.dipankar.com.dipankar.letcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
